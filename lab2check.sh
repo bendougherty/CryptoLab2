bf532f4c0055c68ca828505621a806df175bbad7
bendougherty <74061060+bendougherty@users.noreply.github.com>
2021-11-11 08:06:58 -0500

    Update lab2check.sh
    Changed cmp function to remove the "silent" flag, and updated the condition for the if statement to change "0" to "null".

--- lab2check.sh
+++ lab2check.sh
@@ -8,9 +8,9 @@ filethree="${line}symkey1.bin"
 #decrypt the symmetric key using RSA private key
 openssl rsautl -decrypt -inkey teacherrsakey.pem -in $filetwo -out $filethree
 #compare symmetric key file to the one sent by student in lab 1
-var1=`cmp -s $filethree ../lab1docs/$fileone`
+var1=`cmp $filethree ../lab1docs/$fileone`
 #if the keys match, display "OK", otherwise display "Not OK"
-if [[ $var1 -eq 0 ]]
+if [[ $var1 -eq null ]]
 then
    echo "${line}: OK"
 else
