82bb04f7dc3af8fc821847f4a838f47ff2f0ece3
bendougherty <74061060+bendougherty@users.noreply.github.com>
2022-10-07 14:41:03 -0400

    Fixed error in file comparison
    Changed cmp to diff in line 43.

--- lab2check.sh
+++ lab2check.sh
@@ -40,7 +40,7 @@ filethree="${line}symkey1.bin"
 #decrypt the symmetric key using RSA private key
 openssl rsautl -decrypt -inkey teacherrsakey.pem -in $filetwo -out $filethree
 #compare symmetric key file to the one sent by student in lab 1
-var1=`cmp $filethree ../lab1docs/$fileone`
+var1=`diff $filethree ../lab1docs/$fileone`
 #if the keys match, display "OK", otherwise display "Not OK"
 if [[ -z "$var1" ]]
 then
